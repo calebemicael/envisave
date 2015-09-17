ac83859b125aaec60ac7916481db6f8b1951ee30
Gabrieli Malet <Gabrieli Malet@gabimalet>
2015-09-17 01:03:21 -0300

    Salvei as informações  da classe Postagem, Administrador e Projeto

---
+++ model/Amizade.class.php
@@ -0,0 +1,36 @@
+<?php
+ include_once PATH_ROOT.'/persist/conexao.php';
+
+class Amizade{
+	private $idAmizade;
+        private $idUsu1;
+        private $idUsu2;
+	
+	
+	public function __construct($_idAmizade="", $_idUsu1="", $_idUsu2=""){
+		$this->idAmizade = $_idAmizade;
+		$this->idUsu1 = $_idUsu1;
+		$this->idUsu2 = $_idUsu2;
+        }
+        function getIdAmizade(){
+		return $this->idAmizade;
+	}
+        
+	function setId($_idAmizade){
+		$this->id = $_idAmizade;
+	}
+         function getIdU1(){
+		return $this->idUsu1;
+	}
+        function setId($_idU1){
+		$this->idUsu1 = $_idU1;
+	}
+         function getIdU2(){
+		return $this->idUsu2;
+	}
+        function setIdU3($_idU2){
+		$this->idUsu2 = $_idU2;
+	}
+        
+        // funcao load
+        //funcao de salvar
